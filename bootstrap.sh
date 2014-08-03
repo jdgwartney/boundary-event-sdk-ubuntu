489741cfa441addbbad47387adc5a7d103cb3439
jdgwartney <davidg@boundary.com>
2014-08-03 15:28:49 -0700

    Working files

---
+++ bootstrap.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+
+sudo locale-gen en_US
+sudo apt-get update
+sudo apt-get install software-properties-common python-software-properties
+sudo apt-get install maven
+sudo apt-get install git
+sudo apt-get install oracle-java8-set-default
+touch hello
